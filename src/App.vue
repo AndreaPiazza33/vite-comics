57b0150a2a8cb0c2c97bd23677174b36fe4d1518
AndreaPiazza33 <andreapiazza190201@gmail.com>
2023-09-08 18:17:31 +0200

    feat: add main structure

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script>
 import AppHeader from "./components/AppHeader.vue";
+import AppMain from "./components/AppMain.vue";
 
 export default {
   data() {
@@ -7,13 +8,14 @@ export default {
       title: "",
     };
   },
-  components: { AppHeader },
+  components: { AppHeader, AppMain },
 };
 </script>
 
 <template>
   <div class="container">
     <AppHeader></AppHeader>
+    <AppMain></AppMain>
   </div>
 </template>
 
---
+++ src/components/AppMain.vue
@@ -0,0 +1,16 @@
+<script>
+export default {};
+</script>
+
+<template>
+  <!--contenuti da inserire-->
+  <div id="bg-main">
+    <h3>---Content goes here---</h3>
+  </div>
+</template>
+<style>
+#bg-main {
+  background-color: hsl(0deg 0% 10.98%);
+  color: white;
+}
+</style>
